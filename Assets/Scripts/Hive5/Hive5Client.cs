ce7b62a299177c7b7178345350edc1a6fde2414f
harry <harry@plattr.com>
2014-03-28 03:07:11 +0900

    카톡 SDK  수정 및 메소드 추

--- Assets/Scripts/Hive5/Hive5Client.cs
+++ Assets/Scripts/Hive5/Hive5Client.cs
@@ -169,10 +169,11 @@ namespace Hive5
 			}));
 		}
 
+
 		/// <summary>
-		/// Updates the kakao friends.
+		/// Updates the friends.
 		/// </summary>
-		/// <param name="">.</param>
+		/// <param name="friend_ids">Friend_ids.</param>
 		/// <param name="callBack">Call back.</param>
 		public void updateFriends(string[] friend_ids, apiCallBack callBack)
 		{
@@ -187,17 +188,18 @@ namespace Hive5
 			StartCoroutine(postHTTP(url, requestBody, callBack));
 		}
 
-
 		/// <summary>
 		/// Updates the kakao friends.
 		/// </summary>
-		/// <param name="">.</param>
+		/// <param name="appFriendsInfo">App friends info.</param>
 		/// <param name="callBack">Call back.</param>
-		public void updateKakaoFriends(object requestBody, apiCallBack callBack)
+		public void updateKakaoFriends(JsonData appFriendsInfo, apiCallBack callBack)
 		{
 			// Hive5 API URL 초기화	
 			var url = initializeUrl("kakao_friends");
-			
+
+			var requestBody = JsonMapper.ToObject ("{\"app_friends_info\":"+appFriendsInfo.ToJson()+"}");
+
 			// 코루틴 WWW 호출
 			StartCoroutine(postHTTP(url, requestBody, callBack));
 		}
@@ -413,6 +415,59 @@ namespace Hive5
 			StartCoroutine (postHTTP (url, requestBody, callback));
 		}
 
+		public void batchDeleteMail(long fromMailId, long toMailId, apiCallBack callback)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl("mails/delete_all");
+			
+			var requestBody = new {
+				from_mail_id	= fromMailId,
+				to_mail_id		= toMailId
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine (postHTTP (url, requestBody, callback));
+		}
+
+
+		/// <summary>
+		/// Attachs the mail tag.
+		/// </summary>
+		/// <param name="mailId">Mail identifier.</param>
+		/// <param name="tags">Tags.</param>
+		/// <param name="callback">Callback.</param>
+		public void attachMailTag(long mailId, string[] tags,  apiCallBack callback)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl(string.Format("mails/{0}/add_tags", mailId));
+			
+			var requestBody = new {
+				tags	= tags
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine (postHTTP (url, requestBody, callback));
+		}
+
+		/// <summary>
+		/// Detachs the mail tag.
+		/// </summary>
+		/// <param name="mailId">Mail identifier.</param>
+		/// <param name="tags">Tags.</param>
+		/// <param name="callback">Callback.</param>
+		public void detachMailTag(long mailId, string[] tags,  apiCallBack callback)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl(string.Format("mails/{0}/remove_tags", mailId));
+			
+			var requestBody = new {
+				tags	= tags
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine (postHTTP (url, requestBody, callback));
+		}
+
 
 		/// <summary>
 		/// Registers the push token.
@@ -484,7 +539,438 @@ namespace Hive5
 			StartCoroutine(postHTTP(url, new {}, callback));
 		}
 
+		/// <summary>
+		/// Gets the configurations.
+		/// </summary>
+		/// <param name="configKeys">Config keys.</param>
+		/// <param name="callback">Callback.</param>
+		public void getConfigurations(string[] configKeys, apiCallBack callback)
+		{
+			var url = initializeUrl ("configurations");
 
+			// Hive5 API 파라미터 셋팅
+			TupleList<string, string> parameters = new TupleList<string, string>();
+			Array.ForEach ( configKeys, key => { parameters.Add( ParameterKey.configKey, key ); });
+
+			StartCoroutine ( getHTTP (url, parameters.data, callback) );
+		}
+
+		/// <summary>
+		/// Logs the specified callback.
+		/// </summary>
+		/// <param name="callback">Callback.</param>
+		public void logs(apiCallBack callback)
+		{
+			var url = initializeUrl("auth/delete");
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, new {}, callback));
+		}
+
+		/// <summary>
+		/// Gifts the item.
+		/// </summary>
+		/// <param name="kakao_user_id">Kakao_user_id.</param>
+		/// <param name="itemKey">Item key.</param>
+		/// <param name="count">Count.</param>
+		/// <param name="mail">Mail.</param>
+		/// <param name="callBack">Call back.</param>
+		public void giftItem(string kakao_user_id, string itemKey, int count, string mail, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl("items/gift");
+			
+			var requestBody = new {
+				kakao_user_id 	= kakao_user_id,
+				Item			= itemKey,
+				count			= count,
+				mail			= mail
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callBack));	
+		}
+
+
+		/// <summary>
+		/// Completes the mission.
+		/// </summary>
+		/// <param name="missionKey">Mission key.</param>
+		/// <param name="callback">Callback.</param>
+		public void completeMission(string missionKey, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format("missions/complete/{0}", missionKey));
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, new {}, callback));	
+		}
+
+		/// <summary>
+		/// Batchs the complete missions.
+		/// </summary>
+		/// <param name="missionKeys">Mission keys.</param>
+		/// <param name="callback">Callback.</param>
+		public void batchCompleteMissions(string[] missionKeys, apiCallBack callback)
+		{
+			var url = initializeUrl ("missions/batch_complete");
+
+			var requestBody = new {
+				keys 	= missionKeys
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callback));	
+		}
+
+		/// <summary>
+		/// Gets the completed missions.
+		/// </summary>
+		/// <param name="callback">Callback.</param>
+		public void getCompletedMissions(apiCallBack callback)
+		{
+			var url = initializeUrl ("missions/completed");
+			
+			// Hive5 API 파라미터 셋팅
+			TupleList<string, string> parameters = new TupleList<string, string>();
+			
+			StartCoroutine ( getHTTP (url, parameters.data, callback) );
+		}
+
+
+		/// <summary>
+		/// Gets the reward.
+		/// </summary>
+		/// <param name="rewardId">Reward identifier.</param>
+		/// <param name="callback">Callback.</param>
+		public void getReward(long rewardId, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format ("rewards/{0}", rewardId));
+			
+			// Hive5 API 파라미터 셋팅
+			TupleList<string, string> parameters = new TupleList<string, string>();
+			
+			StartCoroutine ( getHTTP (url, parameters.data, callback) );
+		}
+
+		/// <summary>
+		/// Applies the reward.
+		/// </summary>
+		/// <param name="rewardId">Reward identifier.</param>
+		/// <param name="deleteMail">If set to <c>true</c> delete mail.</param>
+		/// <param name="callback">Callback.</param>
+		public void applyReward(long rewardId, bool deleteMail, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format ("rewards/apply/{0}", rewardId));
+			
+			// Hive5 API 파라미터 셋팅
+			var requestBody = new {
+				delete_mail 	= deleteMail
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callback));	
+		}
+
+		/// <summary>
+		/// Applies all reward.
+		/// </summary>
+		/// <param name="deleteMail">If set to <c>true</c> delete mail.</param>
+		/// <param name="callback">Callback.</param>
+		public void applyAllReward(bool deleteMail, apiCallBack callback)
+		{
+			var url = initializeUrl ("rewards/apply_all");
+			
+			// Hive5 API 파라미터 셋팅
+			var requestBody = new {
+				delete_mail 	= deleteMail
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callback));	
+		}
+
+		/// <summary>
+		/// Invalidates the reward.
+		/// </summary>
+		/// <param name="rewardId">Reward identifier.</param>
+		/// <param name="deleteMail">If set to <c>true</c> delete mail.</param>
+		/// <param name="callback">Callback.</param>
+		public void invalidateReward(long rewardId, bool deleteMail, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format ("rewards/invalidate/{0}", rewardId));
+			
+			// Hive5 API 파라미터 셋팅
+			var requestBody = new {
+				delete_mail 	= deleteMail
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callback));	
+		}
+
+		/// <summary>
+		/// Submits the score.
+		/// </summary>
+		/// <param name="leaderboardId">Leaderboard identifier.</param>
+		/// <param name="score">Score.</param>
+		/// <param name="callback">Callback.</param>
+		public void submitScore(long leaderboardId, long score, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format ("leaderboards/{0}/scores/{1}", leaderboardId, score));
+			
+			// Hive5 API 파라미터 셋팅
+			var requestBody = new {
+				leaderboard_id 	= leaderboardId,
+				score			= score
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callback));	
+		}
+
+		/// <summary>
+		/// Gets the scores.
+		/// </summary>
+		/// <param name="leaderboardId">Leaderboard identifier.</param>
+		/// <param name="itemKeys">Item keys.</param>
+		/// <param name="userDataKeys">User data keys.</param>
+		/// <param name="rankMin">Rank minimum.</param>
+		/// <param name="rankMax">Rank max.</param>
+		/// <param name="rangeMin">Range minimum.</param>
+		/// <param name="rangeMax">Range max.</param>
+		/// <param name="callback">Callback.</param>
+		public void getScores(long leaderboardId, string[] itemKeys, string[] userDataKeys, long rankMin, long rankMax, long rangeMin, long rangeMax, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format ("leaderboards/{0}/my_score", leaderboardId));
+			
+			// Hive5 API 파라미터 셋팅
+			TupleList<string, string> parameters = new TupleList<string, string>();
+			parameters.Add ("leaderboard_id", leaderboardId.ToString());
+			parameters.Add ("range_min", rangeMin.ToString());
+			parameters.Add ("range_max", rangeMax.ToString());
+			
+			StartCoroutine ( getHTTP (url, parameters.data, callback) );
+		}
+
+		/// <summary>
+		/// Gets my score.
+		/// </summary>
+		/// <param name="leaderboardId">Leaderboard identifier.</param>
+		/// <param name="rangeMin">Range minimum.</param>
+		/// <param name="rangeMax">Range max.</param>
+		/// <param name="callback">Callback.</param>
+		public void getMyScore(long leaderboardId, long rangeMin, long rangeMax, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format ("leaderboards/{0}/my_score", leaderboardId));
+			
+			// Hive5 API 파라미터 셋팅
+			TupleList<string, string> parameters = new TupleList<string, string>();
+			parameters.Add ("leaderboard_id", leaderboardId.ToString());
+			parameters.Add ("range_min", rangeMin.ToString());
+			parameters.Add ("range_max", rangeMax.ToString());
+
+			StartCoroutine ( getHTTP (url, parameters.data, callback) );
+		}
+
+		/// <summary>
+		/// Gets the social scores.
+		/// </summary>
+		/// <param name="leaderboardId">Leaderboard identifier.</param>
+		/// <param name="itemKeys">Item keys.</param>
+		/// <param name="userDataKeys">User data keys.</param>
+		/// <param name="callback">Callback.</param>
+		public void getSocialScores(long leaderboardId, string[] itemKeys, string[] userDataKeys, apiCallBack callback)
+		{
+			var url = initializeUrl (string.Format ("leaderboards/{0}/social_scores", leaderboardId));
+			
+			// Hive5 API 파라미터 셋팅
+			TupleList<string, string> parameters = new TupleList<string, string>();
+			parameters.Add ("leaderboard_id", leaderboardId.ToString());
+			Array.ForEach ( itemKeys, key => { parameters.Add( ParameterKey.itemKey, key ); });
+			Array.ForEach ( userDataKeys, key => { parameters.Add( ParameterKey.userDataKey, key ); });
+			
+			StartCoroutine ( getHTTP (url, parameters.data, callback) );
+		}
+
+		/// <summary>
+		/// Gets the friends info.
+		/// </summary>
+		/// <param name="platformUserId">Platform user identifier.</param>
+		/// <param name="itemKeys">Item keys.</param>
+		/// <param name="userDataKeys">User data keys.</param>
+		/// <param name="callback">Callback.</param>
+		public void getFriendsInfo(string[] platformUserIds, string[] itemKeys, string[] userDataKeys, apiCallBack callback)
+		{
+			var url = initializeUrl ("friends/info");
+			
+			// Hive5 API 파라미터 셋팅
+			TupleList<string, string> parameters = new TupleList<string, string>();
+			Array.ForEach ( platformUserIds, key => { parameters.Add( "platform_user_id", key ); }); 
+			Array.ForEach ( itemKeys, key => { parameters.Add( ParameterKey.itemKey, key ); });
+			Array.ForEach ( userDataKeys, key => { parameters.Add( ParameterKey.userDataKey, key ); });
+
+			StartCoroutine ( getHTTP (url, parameters.data, callback) );
+		}
+
+		/// <summary>
+		/// Prize the specified leaderboardId and callBack.
+		/// </summary>
+		/// <param name="leaderboardId">Leaderboard identifier.</param>
+		/// <param name="callBack">Call back.</param>
+		public void prize(string leaderboardId, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl(string.Format("leaderboards/prize/{0}", leaderboardId));
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, new {}, callBack));	
+		}
+
+		/// <summary>
+		/// Creates the apple purchase.
+		/// </summary>
+		/// <param name="productCode">Product code.</param>
+		/// <param name="receiverKakaoUserId">Receiver kakao user identifier.</param>
+		/// <param name="mailForReceiver">Mail for receiver.</param>
+		/// <param name="callBack">Call back.</param>
+		public void createApplePurchase(string productCode, string receiverKakaoUserId, string mailForReceiver, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl("apple_purchases");
+			
+			var requestBody = new {
+				product_code 			= productCode,
+				receiver_kakao_user_id	= receiverKakaoUserId,
+				mail_for_receiver		= mailForReceiver
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callBack));	
+		}
+
+
+		/// <summary>
+		/// Creates the google purchase.
+		/// </summary>
+		/// <param name="productCode">Product code.</param>
+		/// <param name="receiverKakaoUserId">Receiver kakao user identifier.</param>
+		/// <param name="mailForReceiver">Mail for receiver.</param>
+		/// <param name="callBack">Call back.</param>
+		public void createGooglePurchase(string productCode, string receiverKakaoUserId, string mailForReceiver, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl("google_purchases");
+			
+			var requestBody = new {
+				product_code 			= productCode,
+				receiver_kakao_user_id	= receiverKakaoUserId,
+				mail_for_receiver		= mailForReceiver
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callBack));	
+		}
+
+
+		/// <summary>
+		/// Creates the naver purchase.
+		/// </summary>
+		/// <param name="productCode">Product code.</param>
+		/// <param name="payement_sequence">Payement_sequence.</param>
+		/// <param name="callBack">Call back.</param>
+		public void createNaverPurchase(string productCode, string payement_sequence, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl("naver_purchases");
+			
+			var requestBody = new {
+				product_code 		= productCode,
+				payement_sequence	= payement_sequence
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callBack));	
+		}
+
+
+		/// <summary>
+		/// Completes the naver purchase.
+		/// </summary>
+		/// <param name="id">Identifier.</param>
+		/// <param name="listPrice">List price.</param>
+		/// <param name="purchasedPrice">Purchased price.</param>
+		/// <param name="currency">Currency.</param>
+		/// <param name="callBack">Call back.</param>
+		public void completeNaverPurchase(long id, long listPrice, long purchasedPrice, string currency, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl(string.Format("naver_purchases/complete/{0}", id));
+			
+			var requestBody = new {
+				list_price 		= listPrice,
+				purchased_price	= purchasedPrice,
+				currency		= currency
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callBack));	
+		}
+
+
+		/// <summary>
+		/// Completes the google purchase.
+		/// </summary>
+		/// <param name="id">Identifier.</param>
+		/// <param name="listPrice">List price.</param>
+		/// <param name="purchasedPrice">Purchased price.</param>
+		/// <param name="currency">Currency.</param>
+		/// <param name="purchaseData">Purchase data.</param>
+		/// <param name="signature">Signature.</param>
+		/// <param name="callBack">Call back.</param>
+		public void completeGooglePurchase(long id, long listPrice, long purchasedPrice, string currency, string purchaseData, string signature, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl(string.Format("google_purchases/complete/{0}", id));
+			
+			var requestBody = new {
+				list_price 		= listPrice,
+				purchased_price	= purchasedPrice,
+				currency		= currency,
+				purchase_data	= purchaseData,
+				signature		= signature
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callBack));	
+		}
+
+
+		/// <summary>
+		/// Completes the apple purchase.
+		/// </summary>
+		/// <param name="id">Identifier.</param>
+		/// <param name="listPrice">List price.</param>
+		/// <param name="purchasedPrice">Purchased price.</param>
+		/// <param name="currency">Currency.</param>
+		/// <param name="receipt">Receipt.</param>
+		/// <param name="isSandbox">If set to <c>true</c> is sandbox.</param>
+		/// <param name="callBack">Call back.</param>
+		public void completeApplePurchase(long id, long listPrice, long purchasedPrice, string currency, string receipt, bool isSandbox, apiCallBack callBack)
+		{
+			// Hive5 API URL 초기화
+			var url = initializeUrl(string.Format("apple_purchases/complete/{0}", id));
+
+			var requestBody = new {
+				list_price 		= listPrice,
+				purchased_price	= purchasedPrice,
+				currency		= currency,
+				receipt			= receipt,
+				is_sandbox		= isSandbox
+			};
+			
+			// 코루틴 WWW 호출
+			StartCoroutine(postHTTP(url, requestBody, callBack));	
+		}
 
 
 
--- Assets/Scripts/Hive5SampleUI/Friends.cs
+++ Assets/Scripts/Hive5SampleUI/Friends.cs
@@ -7,59 +7,42 @@ public class Friends : MonoBehaviour {
 
 	Hive5Client H5;
 	
+
 	/// <summary>
-	/// Sets the user data.
+	/// Updates the friends.
 	/// </summary>
 	public void updateFriends()
 	{
-		H5 = Hive5Client.Instance;	// Hive5Client 호출
-
-		var requestBody = new {
-			app_friends_info = new[] {
-				new {
-					user_id = "88197948207226176",
-					nickname = "heesung",
-					profile_image_url = "",
-					friend_nickname = "heesung", 
-					message_blocked = false,
-					hashed_talk_user_id = "123123"
-				}
-			}
-		};
-
+		H5 = Hive5Client.Instance;    // Hive5Client 호출
+		
+		var friend_ids = new string[] {"881979482072261763"};
+		
 		Hive5Client.apiCallBack callBack = onUpdateFriends;
-		H5.updateKakaoFriends (requestBody, callBack);
-
+		H5.setZone (Hive5APIZone.Beta);
+		H5.setDebug ();
+		H5.updateFriends (friend_ids, callBack);
 	}
 
-	/// <summary>
-	/// Sets the user data.
-	/// </summary>
-	public void updateKakaoFriends()
-	{
-		H5 = Hive5Client.Instance;	// Hive5Client 호출
-		
-		var requestBody = new {
-			app_friends_info = new[] {
-				new {
-					user_id = "88197948207226176",
-					nickname = "heesung",
-					profile_image_url = "",
-					friend_nickname = "heesung", 
-					message_blocked = false,
-					hashed_talk_user_id = "123123"
-				}
-			}
-		};
-		
-		Hive5Client.apiCallBack callBack = onUpdateKakaoFriends;
-		H5.updateKakaoFriends (requestBody, callBack);
-		
-	}
+/// <summary>
+/// Updates the kakao friends.
+/// </summary>
+public void updateKakaoFriends()
+{
+	H5 = Hive5Client.Instance;	// Hive5Client 호출
+
+	string kakaoResponseString = "[{\"user_id\":\"88308950770094417\", \"hashed_talk_user_id\":\"AQkNc3MNCQE\", \"message_blocked\":\"false\", \"profile_image_url\":\"http://th-p17.talk.kakao.co.kr/th/talkp/wkdl47x1q9/VzwE5eCc0Vjuzk9xgMEfUk/dhjdcp_110x110_c.jpg\", \"nickname\":\"이승근\"},{\"user_id\":\"88258809308309793\", \"hashed_talk_user_id\":\"AAJGo6NGAgA\", \"message_blocked\":\"false\", \"profile_image_url\":\"http://th-p4.talk.kakao.co.kr/th/talkp/wkbzYhjI5O/oxJZKuK332UZzMAW9YOWXk/dcwz9g_110x110_c.jpg\", \"nickname\":\"쭌\"}]";
+
+	var appFriendsInfo = (JsonMapper.ToObject (kakaoResponseString));
+
+	Hive5Client.apiCallBack callBack = onUpdateKakaoFriends;
+	H5.setDebug ();
+	H5.updateKakaoFriends (appFriendsInfo, callBack);
 	
+}
+		
 
 	/// <summary>
-	/// Ons the set user data.
+	/// Ons the update friends.
 	/// </summary>
 	/// <param name="resultCode">Result code.</param>
 	/// <param name="response">Response.</param>
@@ -80,7 +63,7 @@ public class Friends : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// Ons the set user data.
+	/// Ons the update kakao friends.
 	/// </summary>
 	/// <param name="resultCode">Result code.</param>
 	/// <param name="response">Response.</param>

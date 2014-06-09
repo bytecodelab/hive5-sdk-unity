d3d6991e92b44904b139303c4342c0672d94edbc
Gilbok Lee <gilbok@live.com>
2014-06-09 14:37:37 +0900

    Switch Platform API를 SDK에 구

---
+++ Assets/Hive5/Model/ResponseBody/SwitchPlatformResponseBody.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using LitJson;
+using Hive5;
+using Hive5.Util;
+
+namespace Hive5.Model
+{
+	
+	/// <summary>
+	/// Login data.
+	/// </summary>
+	public class SwitchPlatformResponseBody : IResponseBody
+	{
+		public int 		ResultCode;
+		public string 	ResultMessage;
+
+		/// <summary>
+		/// Load the specified json.
+		/// </summary>
+		/// <param name="json">Json.</param>
+		public static SwitchPlatformResponseBody Load(JsonData json)
+		{
+			
+			if (json == null)
+				return null;
+			
+			var resultCode 			= (int)json["result_code"];
+			var resultMessage 		= (string)json["result_message"];
+
+			return new SwitchPlatformResponseBody ()
+			{
+				ResultCode = resultCode, 
+				ResultMessage = resultMessage,
+			};
+		}
+		
+	}
+}
